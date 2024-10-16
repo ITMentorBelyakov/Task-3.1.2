aeccde4bedf9496cddacc2423054ce6c0a6fee6b
ClassIc52 <diman1454@gmail.com>
2024-10-16 14:44:38 +0300

    Добавил BCrypt, исправил цикличную зависимость, добавил добовление роли Администратором

--- src/main/java/ru/itmentor/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -2,7 +2,6 @@ package ru.itmentor.spring.boot_security.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -11,10 +10,5 @@ public class SpringBootSecurityDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
-
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		String encodedPassword = encoder.encode("password");
-		System.out.println(encodedPassword);
 	}
-
 }
